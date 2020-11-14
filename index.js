c735b956a0c79277b97375d965812d49a0c156da
François Billioud <f.billioud@gmail.com>
2020-11-14 00:49:58 -0300

    First working CLI version
    Signed-off-by: François Billioud <f.billioud@gmail.com>

--- index.js
+++ index.js
@@ -4,50 +4,73 @@
 /** @type {import('axios').default} */
 // @ts-ignore
 const axios = require('axios')
+/** @type {import('readline')} */
+const readline = require('readline')
 
 const PORT = process.env.PORT || 80
 const url = `http://localhost:${PORT}`
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  completer
+})
 
-console.log(`Requests will reach ${url}. To change the port, edit PORT env variable.`)
+console.log(`\x1b[90mRequests will reach ${url}. To change the port, edit PORT env variable.\x1b[0m`)
+console.log('Use \x1b[1mhelp\x1b[0m to see the available commands')
 
-// eslint-disable-next-line no-unused-vars
-const [node, file, command, parameter] = process.argv
-
-switch (command) {
-  case 'login': login(parameter); break
-  case 'req':
-  case 'request':
-    request(parameter)
-    break
-  case 'jwt': setJWT(parameter); break
-  default: help(); break
+function completer (line) {
+  const completions = 'help login request jwt'.split(' ')
+  const hits = completions.filter((c) => c.startsWith(line))
+  // Show all completions if none found
+  return [hits.length ? hits : completions, line]
 }
 
+rl.on('line', (line) => {
+  const [command, ...rest] = line.split(' ')
+  const req = rest.join(' ')
+  switch (command) {
+    case 'login': login(req); break
+    case 'req':
+    case 'request':
+      request(req)
+      break
+    case 'jwt': setJWT(req); break
+    default: help(); break
+  }
+})
+
 function help () {
-  console.log(`You can use the following commands:
+  console.log(`\x1b[34mYou can use the following commands:
     - jwt: {string?} Set a jwt or remove it if empty
     - req: {string} Send a SimpleQL request
-    - login: {string} Retrieve the jwt token from the next request
+    - login: {string} Retrieve the jwt token from the next request and adds it to the next requests
     - help: This help message
-  `)
+    \x1b[0m`)
 }
 
 /**
  * Send a request and retrieve the jwt token from it
  * @param {string} req The login request. Should look like { User: { login: 'test', password: 'test' }}
  */
-function login (req) {
-  return request(req).then(res => {
-    const result = res.data
-    const tableName = Object.keys(result).find(table => Array.isArray(table) && table.length && table[0].jwt)
+async function login (req) {
+  try {
+    const response = await sendRequest(req)
+    const result = response.data
+    const tableName = Object.keys(result).find(table => Array.isArray(result[table]) && result[table].length && result[table][0].jwt)
+
     if (!tableName) {
-      console.log(`We couldn't find the jwt after login. Maybe the request failed. Here is the results: ${JSON.stringify(result, null, 4)}`)
+      logError(`We couldn't find the jwt after login. Maybe the request failed. Here is the results: ${JSON.stringify(result, null, 4)}`)
     } else {
-      const { jwt, reservedId } = result[tableName].jwt
+      const { jwt, reservedId } = result[tableName][0]
       setJWT(jwt)
-      console.log(`User ${reservedId} got logged in.`)
+      logSuccess(`User ${reservedId} got logged in.`)
     }
-  })
+  } catch (err) {
+    /** @type {import('axios').AxiosError} **/
+    const error = err
+    if (!error.isAxiosError) logError(error.message)
+    else logError(error.response.data)
+  }
 }
 
 /**
@@ -55,8 +78,27 @@ function login (req) {
  * @param {object} req The simple-ql request
  * @returns {Promise<import('axios').AxiosResponse>}
  */
-function request (req) {
-  return axios.post(url, req)
+function sendRequest (req) {
+  const correctJson = req.replace(/(['"])?([a-z0-9A-Z_]+)(['"])?:/g, '"$2": ')
+  const correctQuotes = correctJson.replace(/'([^\s,{[\]}]+)'/g, '"$1"')
+  return axios.post(url, JSON.parse(correctQuotes))
+}
+
+/**
+ * Send a SimplQL request to the endpoint
+ * @param {object} req The simple-ql request
+ * @returns {Promise<void>}
+ */
+async function request (req) {
+  try {
+    const result = await sendRequest(req)
+    console.log(result.data)
+  } catch (err) {
+    /** @type {import('axios').AxiosError} **/
+    const error = err
+    if (!error.isAxiosError) logError(error.message)
+    else logError(error.response.data)
+  }
 }
 
 /**
@@ -64,7 +106,27 @@ function request (req) {
  * @param {string?} jwt The jwt token or undefined to remove
  */
 function setJWT (jwt) {
-  jwt
-    ? (axios.defaults.headers.common.Authorization = 'Bearer ' + jwt)
-    : delete axios.defaults.headers.common.Authorization
+  try {
+    jwt
+      ? axios.defaults.headers.common.Authorization = 'Bearer ' + jwt
+      : delete axios.defaults.headers.common.Authorization
+  } catch (err) {
+    if (err.code !== 'ENOENT') throw err
+  }
+}
+
+/**
+ * Log an error message
+ * @param {string} message The message to log
+ */
+function logError (message) {
+  console.log('\x1b[31m', message, '\x1b[0m')
+}
+
+/**
+ * Log a success message
+ * @param {string} message The message to log
+ */
+function logSuccess (message) {
+  console.log('\x1b[32m', message, '\x1b[0m')
 }
